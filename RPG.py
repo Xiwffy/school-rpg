7d0b0114eeed364bee7bf2ee74b8c05ff32970ab
Xiwffy <165550397+Xiwffy@users.noreply.github.com>
2024-04-03 16:34:06 +0300

    Update RPG.py
    Add start scene

--- RPG.py
+++ RPG.py
@@ -1,20 +1,48 @@
 from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QVBoxLayout, QPushButton, QLabel, QTextEdit, QLineEdit, QListWidget, QInputDialog, QGroupBox
 from PyQt5.QtCore import Qt
+from PIL import Image
+from PyQt5.QtGui import QPixmap
+
 
 def vstrecha():
-    sos.hide()
-    start.setLayout(box_line)
-    start.show()
+    print(start_bottom.text())
+    if start_bottom.text() == 'START!':
+        name_start.setText('Приветсвую тебя в твоей игре! \nКак ты знаешь в школе часто бывает скучно и хотелось бы немного веселья и разнообразия\nРешение этого данная "игра" которая поднимет твою мотивацию!')
+        start_bottom.setText('Дальше»')
+    elif start_bottom.text() == 'Дальше»':
+        loadImage()
+        start_bottom.setText('Начнём!')
+    elif start_bottom.text() == 'Начнём!':
+        dude.setText('Всё разъяснили\nПора приступать!')
+        w, h = 300, 500
+        loadImage()
+
+
+
+
+def loadImage():
+    pixmapimage = QPixmap('puro.png')
+    w, h = 300, 500
+    pixmapimage = pixmapimage.scaled(w, h)
+    name_start.setPixmap(pixmapimage)
+    name_start.setAlignment(Qt.AlignRight)
+    dude.show()
+    
+
+
+
+
 app = QApplication([])
 win = QWidget()
 
-sos = QGroupBox('')
-start = QGroupBox('Начало')
+
 name_start = QLabel('ДОБРО ПОЖАЛОВАТЬ НА ВАШУ ШКОЛЬНУЮ РПГ!')
+
+dude = QLabel('Это твой дружище\nПри помощи твоих знаний ты усилишь его\nНапример: Сделал домашку по матиматике - +5 баллов к уму\nНаписал контрольную работу на 5 - +40 к ХП и тд')
+
+
 start_bottom = QPushButton('START!')
 name_start.setAlignment(Qt.AlignCenter)
-
-s_label = QLabel('Приветсвую тебя в твоей игре! \nКак ты знаешь в школе часто бывает скучно и хотелось бы немного веселья и разнообразия\nРешение этого данная "игра" которая поднимет твою мотивацию!')
 
 box_line = QVBoxLayout()
 V_Line = QVBoxLayout()
@@ -22,8 +50,7 @@ V2_Line = QVBoxLayout()
 H_Line = QHBoxLayout()
 H2_Line = QHBoxLayout()
 
-sos.setLayout(H_Line)
-box_line.addWidget(s_label)
+V2_Line.addWidget(dude)
 V2_Line.addWidget(name_start)
 V2_Line.addWidget(start_bottom)
 H_Line.addLayout(V_Line)
@@ -34,6 +61,7 @@ win.setLayout(box_line)
 
 
 start_bottom.clicked.connect(vstrecha)
+
 #start.setLayout(H_Line)
 
 
@@ -57,8 +85,7 @@ start_bottom.clicked.connect(vstrecha)
 
 
 
-
-
+dude.hide()
 win.resize(900, 600)
 win.setWindowTitle('...---...')
 win.show()
